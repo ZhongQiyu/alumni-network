cd3300587ae33d3bc03a0ef3a5537e4f20788924
ZhongQiyu <xiaoyu991214@gmail.com>
2024-08-05 07:59:53 -0700

    update app.py

--- src/app.py
+++ src/app.py
@@ -93,3 +93,49 @@ log_file = os.path.join(project_dir, 'temp', 'OneDC_Updater', 'update.log')
 with open(log_file, 'r') as file:
     log_contents = file.read()
     st.text(log_contents)
+
+import streamlit as st
+
+# 设置应用标题
+st.title("Babson China Club Alumni Card")
+
+# 个人中心部分
+st.header("个人中心")
+login_button = st.button("点击登录账号")
+
+if login_button:
+    st.info("登录功能待实现")
+
+# 校友卡部分
+st.header("百森俱乐部校友卡 Babson China Club")
+get_card_button = st.button("立即领取")
+
+if get_card_button:
+    st.session_state.show_form = True
+
+# 申领表单部分
+if 'show_form' not in st.session_state:
+    st.session_state.show_form = False
+
+if st.session_state.show_form:
+    st.header("表1: 申领校友卡")
+    with st.form("survey_form"):
+        name = st.text_input("姓名")
+        contact = st.text_input("联系方式")
+        occupation = st.text_input("职业信息")
+        submit_button = st.form_submit_button("提交")
+
+        if submit_button:
+            st.success("表单提交成功")
+            # 这里可以处理表单数据，例如发送到服务器
+            st.session_state.show_form = False
+
+# 校友会联系方式部分
+st.header("校友会联系方式：")
+st.write("XXXXXXXXXXXXX")
+
+# 显示 main.html 的内容
+st.header("展示 main.html 内容")
+with open("main.html", "r", encoding="utf-8") as file:
+    html_content = file.read()
+st.components.v1.html(html_content, height=600)
